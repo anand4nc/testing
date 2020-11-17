2f05c3624239ab0fca9af8ecbfa43e67fb2d1d44
anand <anand4nc@gmail.com>
2020-11-17 19:54:10 +0530

    added the jest

---
+++ filterByTerm.js
@@ -0,0 +1,18 @@
+function filterByTerm(inputArr, searchTerm) {
+    if (!searchTerm) throw Error("searchTerm cannot be empty");
+    if (!inputArr.length) throw Error("inputArr cannot be empty"); // new line
+    const regex = new RegExp(searchTerm, "i");
+    return inputArr.filter(function(arrayElement) {
+      return arrayElement.url.match(regex);
+    });
+  }
+  
+  const codeSmell = () => {
+      let a = 5;
+    return a;
+  }
+  
+  module.exports = {
+      filterByTerm,
+      codeSmell
+  }
